d394acd198f1c61db200fc01296477c71e82cf29
KaLa <k.lapienis@gmail.com>
2024-05-21 15:21:07 +0300

    user api bug fix

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ connectToDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/users", require("./routes/userRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
 const PORT = 5000;
 app.listen(PORT, () => {
