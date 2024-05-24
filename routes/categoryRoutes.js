1ae89f74648881b7bacb1bed6269b0d25398b891
Laurynas <sapnagis.laurynasls@gmail.com>
2024-05-24 13:07:22 +0300

    verify/fix

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -18,8 +18,8 @@ const { verifyToken, checkAdminRole } = require("../Middleware/authMiddleware");
 // @ api/categories
 
 router.post("/", verifyToken, checkAdminRole, createCategory); // Ensure only admins can create categories
-router.get("/", verifyToken, getAllCategories);
-router.get("/:id", verifyToken, getCategoryById);
+router.get("/", getAllCategories);
+router.get("/:id", getCategoryById);
 router.delete("/:id", verifyToken, checkAdminRole, deleteCategoryById); // Ensure only admins can delete categories
 
 module.exports = router;
