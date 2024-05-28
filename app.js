4899164567206c2d819692b93f693ea9ec003a1e
KaLa <k.lapienis@gmail.com>
2024-05-29 00:21:59 +0300

    favorites done

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use("/api/categories", require("./routes/categoryRoutes"));
 app.use("/api/ads", require("./routes/adRoutes"));
 app.use("/api/likes", require("./routes/likeRoutes"));
 app.use("/api/comments", require("./routes/commentRoutes"));
+app.use("/api/favorites", require("./routes/favoriteRoutes"));
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
