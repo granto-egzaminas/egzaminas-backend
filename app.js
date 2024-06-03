c2cd861f5413da4bf84b98788c09dfc35df802c1
kestulka <kgirdiunas0@gmail.com>
2024-06-04 02:27:44 +0300

    .

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -16,14 +15,6 @@ app.use("/api/ads", require("./routes/adRoutes"));
 app.use("/api/likes", require("./routes/likeRoutes"));
 app.use("/api/comments", require("./routes/commentRoutes"));
 app.use("/api/favorites", require("./routes/favoriteRoutes"));
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "build")));
-
-// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
